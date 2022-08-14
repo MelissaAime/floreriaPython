fb57a90e0fd6061d7e488a10c71b3b666da86afc
Melissa Aime <aimemelissa1@gmail.com>
2022-08-14 11:33:35 -0300

    create new app for authentication, add styles

--- src/LasFlores/settings.py
+++ src/LasFlores/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'flores',
+    'authentication',
 ]
 
 MIDDLEWARE = [
--- src/LasFlores/urls.py
+++ src/LasFlores/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("flores.urls"))
+    path('', include("flores.urls")),
+    path('section', include("authentication.urls"))
 ]
