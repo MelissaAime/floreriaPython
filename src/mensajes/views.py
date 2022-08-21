253a5cf8bfc008deb482db37a021270221194877
Melissa Aime <aimemelissa1@gmail.com>
2022-08-21 16:57:03 -0300

    login required

--- src/mensajes/views.py
+++ src/mensajes/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from mensajes.models import Mensajes
 from django.views.generic import CreateView, ListView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Mensajes
 
 # Crear mensaje nuevo
-class MensajeNuevo(CreateView):
+class MensajeNuevo(LoginRequiredMixin, CreateView):
     model = Mensajes
     success_url = "/mensajes/leer/"
     fields = ['nombre', 'email', 'mensaje']
 
 # Leer los mensajes
-class MensajeLista(ListView):
+class MensajeLista(LoginRequiredMixin, ListView):
     model = Mensajes
     template_name = "mensajes/mensajes_list.html"
 
 # Borrar los mensajes
-class MensajeBorrar(DeleteView):
+class MensajeBorrar(LoginRequiredMixin, DeleteView):
     model = Mensajes
     success_url = "/mensajes/leer/"
